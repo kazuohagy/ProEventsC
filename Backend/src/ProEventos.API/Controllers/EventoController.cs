c97a9d56371f28a2fa615d579d91ac5bbe415a57
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-11-29 13:24:50 -0300

    Routes basic

---
+++ Backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Evento Get";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Evento Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Evento Put {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Evento Delete {id}";
+        }
+    }
+}

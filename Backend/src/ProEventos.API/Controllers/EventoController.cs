9c29f08da677246cfea3a5b1457513a0c10f1328
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-11-30 02:24:59 -0300

    get and getById

--- Backend/src/ProEventos.API/Controllers/EventoController.cs
+++ Backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ProEventos.Models;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
@@ -12,15 +12,45 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+        public IEnumerable<Evento> _evento = new Evento[]
+            {
+                new Evento()
+                {
+                Id = 1,
+                Tema = "Angular e .NET Core",
+                Local = "São Paulo",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                ImagemURL = "foto.png",
+                Telefone = "11 99999-9999",
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                        new Evento()
+                {
+                Id = 2,
+                Tema = "Angular e novidades",
+                Local = "Paraiba",
+                Lote = "2º Lote",
+                QtdPessoas = 350,
+                ImagemURL = "foto.png",
+                Telefone = "11 99999-9999",
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+                },
+            };
         public EventoController()
         {
 
         }
 
         [HttpGet]
-        public Evento Get()
+        public IEnumerable<Evento> Get()
         {
-            return "Evento Get";
+            return _evento;
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.Id == id);
         }
 
         [HttpPost]

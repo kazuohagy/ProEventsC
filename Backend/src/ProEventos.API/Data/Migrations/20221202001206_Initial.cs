1b4a224bf11953104feef7c52ecc01c6d2908627
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-12-01 21:40:38 -0300

    Migrations and Database

---
+++ Backend/src/ProEventos.API/Data/Migrations/20221202001206_Initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProEventos.API.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Local = table.Column<string>(type: "TEXT", nullable: true),
+                    DataEvento = table.Column<string>(type: "TEXT", nullable: true),
+                    Tema = table.Column<string>(type: "TEXT", nullable: true),
+                    QtdPessoas = table.Column<int>(type: "INTEGER", nullable: false),
+                    ImagemURL = table.Column<string>(type: "TEXT", nullable: true),
+                    Telefone = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    Lote = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eventos");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,21 @@ E necessario rodar esses comandos no terminal:
 ```
 dotnet tool install --global dotnet-ef
 ```
+
+Create migrations
+
+```
+dotnet ef migrations add Initial -o Data/Migrations
+```
+
+Para remover migration
+
+```
+dotnet ef migrations remove
+```
+
+Para criar um banco de dados SQLITE
+
+```
+dotnet ef database update
+```
